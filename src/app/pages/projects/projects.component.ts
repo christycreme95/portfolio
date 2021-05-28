6bf1475595f3a42926b3d7f06952d9e853f729cc
Christy Eang <christyeang@yahoo.com>
2021-05-27 22:18:17 -0700

    playing with project page

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { url } from 'node:inspector';
 
 @Component({
   selector: 'app-projects',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+   projects = [
+     {
+       name:'Mayor Of Stockton', 
+       picture: '../../../assets/images/proj/mayorStockton.png', 
+       description: 'Built and launched the official site of the mayor of Stockton.', 
+       src: 'https://www.stocktonmayor.org', 
+       keywords: "design, build, html, css, bootstrap"
+      },
 
+     {
+       name:'Triton', 
+       picture: '', 
+       description: 'In Progress: Full stack mobile app to keep track of your pets!', 
+       src: '', 
+       keywords: " build, teamwork, ionic, angular, scss, bootstrap"
+      },
+     {
+       name:'Amor Kombucha', 
+       picture: '', 
+       description: 'Local Business Design of Amor Kombuchas Website.', 
+       src: 'https://www.eang.dev/amor', 
+       keywords: "design, build, html, css, javascript, bootstrap"
+      },
+     {
+       name:'', picture: '', description: '', src: '', keywords: ""
+      },
+
+   ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  
 }
